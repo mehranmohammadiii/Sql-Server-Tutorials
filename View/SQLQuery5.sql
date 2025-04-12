b9dc94517d45b335da4f5c758b8a5c590a0625b5
mehranmohammadiii <mehranmohammaadiii@gmail.com>
2025-04-12 17:25:59 +0330

    Create View

---
+++ View/SQLQuery5.sql
@@ -0,0 +1,35 @@
+﻿--View
+--کد ساخت دیتابیس dbshop__DjiKala در کوِئری های قبلی هست
+create view V_Test1
+as
+select *
+from Products
+where Price>100000;
+
+select *
+from V_Test1;
+
+select *
+from V_Test1
+where ProductGroupId=10;
+
+alter view V_Test1  --تغییر و آپدیت
+as
+select *
+from Products
+where Price>25000;
+
+select *
+from V_Test1
+
+drop view if exists V_Test1
+
+exec sp_rename 'V_Test1' , 'V_Test'
+
+select *
+from sys.tables
+
+select *
+from sys.views
+
+exec sp_helptext V_Test
